e5728bd32fd8fe53f81a068acc6a2493abf70c3d
Kamil Butryn <kaybenot@gmail.com>
2024-10-26 15:49:37 +0200

    Gillotine remove limb

--- Assets/Scripts/Interactables/Gillotine.cs
+++ Assets/Scripts/Interactables/Gillotine.cs
@@ -2,4 +2,13 @@ using UnityEngine;
 
 public class Gillotine : AnimationInteractableBase
 {
+    public override void interact(Player user)
+    {
+        base.interact(user);
+
+        if (Player.Instance != null)
+        {
+            Player.Instance.RemoveLimbOrdered();
+        }
+    }
 }
