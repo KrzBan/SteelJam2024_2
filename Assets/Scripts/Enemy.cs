c69a8a31f3202297e11cd6b7009c2cc4bd989831
SzymonKonieczny <72419029+SzymonKonieczny@users.noreply.github.com>
2024-10-26 16:50:38 +0200

    enemy udpate

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,22 +1,31 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent), typeof(Animator))]
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour, IDamagable
 {
+
+    enum EnemyState
+    {
+        Following,
+        Attacking
+    }
     public Transform target;
-    
+    public LayerMask m_LayerMask;
     private NavMeshAgent _agent;
     private Animator _animator;
     private float _lastUpdate = 0.0f;
     private float _updateIntervalFar = 5.0f;
     private float _updateIntervalShort = 1.0f;
     private float _shortDistance = 4.0f;
-
+    private EnemyState _state;
+    private float _stateSwitchCooldown=0;
     void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
+        _state = EnemyState.Following;
     }
 
     void Update()
@@ -24,15 +33,59 @@ public class Enemy : MonoBehaviour
         _animator.SetFloat("Speed", _agent.velocity.magnitude);
         
         if (target == null) return;
-        
-        var interval = (transform.position - target.position).magnitude > _shortDistance 
-            ? _updateIntervalFar : _updateIntervalShort;
-        if( Time.time > _lastUpdate + interval)   
+
+        _stateSwitchCooldown -= Time.deltaTime;
+        switch(_state)
         {
-            _lastUpdate = Time.time;
-            _agent.SetDestination(target.position);
+            case EnemyState.Following :
+
+                var interval = (transform.position - target.position).magnitude > _shortDistance
+            ? _updateIntervalFar : _updateIntervalShort;
+                if (Time.time > _lastUpdate + interval)
+                {
+                    _lastUpdate = Time.time;
+                    _agent.SetDestination(target.position);
+                }
+                if(Vector3.Distance(transform.position,target.position) < 1f
+                    && _stateSwitchCooldown <=0)
+                {
+                    _state = EnemyState.Attacking;
+                    _stateSwitchCooldown = 2f;
+                    _animator.SetBool("Attack", true);
+                    StartCoroutine(Attack());
+                    _agent.Stop();
+                }
+
+                break;
+            case EnemyState.Attacking:
+                if(_stateSwitchCooldown <=0)
+                {
+                    _state = EnemyState.Following;
+                    _agent.Resume();
+                }
+
+
+                break;
         }
+        
     }
+    IEnumerator Attack()
+    {
+
+
+        yield return new WaitForSeconds(1f);
+
+
+        Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2, Quaternion.identity, m_LayerMask);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            Debug.Log("Hit : " + hitColliders[i].name + i);
+
+            i++;
+        }
+
+    }    
     
     public void SetTarget(Transform target)
     {
