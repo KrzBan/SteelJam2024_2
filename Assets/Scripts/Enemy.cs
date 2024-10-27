3fa2a0a9db250065a45f0ab1ee748abb8a07ce31
Krzysztof <krzys0100@wp.pl>
2024-10-27 16:44:58 +0100

    Guillotine blood splatter

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Cinemachine;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
